d2e62be86692c56f7dd8b5483ca1b488587e7a32
Jonas De Winter <jonas.dewinter@student.ucll.be>
2020-02-18 17:05:03 +0100

    productDB en cd

--- lab1/src/Model/CD.java
+++ lab1/src/Model/CD.java
@@ -1,4 +1,13 @@
 package Model;
 
-public class CD {
+public class CD extends Product{
+    private double prijs = 1.5;
+
+    public CD(String title, int id){
+        super(title, id);
+    }
+
+    public double getPrice(int dagen){
+        return prijs * dagen;
+    }
 }
