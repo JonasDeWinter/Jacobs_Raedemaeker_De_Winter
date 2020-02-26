28c509f7dc3ce97a46e12a240db2fce6a4af5b7e
Jonas De Winter <jonas.dewinter@student.ucll.be>
2020-02-26 14:10:00 +0100

    ja da werkt ni he

--- Lab2/src/CaesarStrategy.java
+++ Lab2/src/CaesarStrategy.java
@@ -10,7 +10,7 @@ public class CaesarStrategy implements CodeerStrategy{
             for(int j = 0; j< alfabet.length(); j++){
                 if(tekst.charAt(i) == (alfabet.charAt(j))){
                     if(j-3<0){
-                        uitkomst += alfabet.charAt(25+(j-3));
+                        uitkomst += alfabet.charAt(26+(j-3));
                     }else {
                         uitkomst += alfabet.charAt(j - 3);
                     }
@@ -22,12 +22,32 @@ public class CaesarStrategy implements CodeerStrategy{
                 uitkomst += s;
             }
         }
+
         return uitkomst;
     }
 
     @Override
     public String decode(String tekst){
-        System.out.println(tekst);
-        return" ";
+        String uitkomst = "";
+        tekst = tekst.toLowerCase();
+        for(int i = 0; i< tekst.length(); i++){
+
+            for(int j = 0; j< alfabet.length(); j++){
+                if(tekst.charAt(i) == (alfabet.charAt(j))){
+                    if(j+3>25){
+                        uitkomst += alfabet.charAt(25-j);
+                    }else {
+                        uitkomst += alfabet.charAt(j + 3);
+                    }
+                }
+
+            }
+            String s = String.valueOf(tekst.charAt(i));
+            if(!alfabet.contains(s)){
+                uitkomst += s;
+            }
+        }
+        System.out.println(uitkomst);
+        return uitkomst;
     }
 }
